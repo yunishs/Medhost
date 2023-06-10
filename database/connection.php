d6f50e2ac36debf760c878308ef3b17f8b0da631
prasi09 <prasiddha.shiwakoti@gmail.com>
2023-06-11 01:09:37 +0545

    int

---
+++ database/connection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername= 'localhost';
+    $username= 'root';
+    $password= '';
+
+    //Connecting to database
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=login-info", $username, $password);
+        //set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Connection Successfully.';
+    }catch(\Exception $e){
+        $error_message=$e->getMessage();
+    }
+
+?>
