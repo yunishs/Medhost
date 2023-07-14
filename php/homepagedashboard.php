9775c7423522217f738c58693cf467fbf03e82e3
Fchhetry <poojafuyal20@gmail.com>
2023-07-14 16:18:33 +0545

    co

--- php/homepagedashboard.php
+++ php/homepagedashboard.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="homepagedashboard.css">
+    <link rel="stylesheet" href="..\public\homepagedashboard.css">
      
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
@@ -15,41 +15,8 @@
 </head>
 <body>
     <nav>
-        <div class="logo-name">
-            <div class="logo-image">
-               <img src="images/logo.png" alt="">
-            </div>
-            <span class="logo_name">DASHBOARD</span>
-        </div>
-        <div class="menu-items">
-            <ul class="nav-links">
-                <li><a href="#">
-                    <i class="uil uil-estate"></i>
-                    <span class="link-name">Dahsboard</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-files-landscapes"></i>
-                    <span class="link-name">Content</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-chart"></i>
-                    <span class="link-name">Analytics</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-thumbs-up"></i>
-                    <span class="link-name">Like</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-comments"></i>
-                    <span class="link-name">Comment</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-share"></i>
-                    <span class="link-name">Share</span>
-                </a></li>
-            </ul>
-            
-            <ul class="logout-mode">
+        <div>
+             <ul class="logout-mode">
                 <li><a href="#">
                     <i class="uil uil-signout"></i>
                     <span class="link-name">Logout</span>
@@ -69,13 +36,15 @@
     <section class="dashboard">
         <div class="top">
             <i class="uil uil-bars sidebar-toggle"></i>
+        <!--
             <div class="search-box">
                 <i class="uil uil-search"></i>
                 <input type="text" placeholder="Search here...">
             </div>
             
-            <!--<img src="images/profile.jpg" alt="">-->
+            <img src="images/profile.jpg" alt="">-->
         </div>
+        
         <div class="dash-content">
             <div class="overview">
                 <div class="title">
