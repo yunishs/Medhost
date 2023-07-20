6b9e26b64a37a1e60eb399876ecbf708571a8ac1
Fchhetry <poojafuyal20@gmail.com>
2023-07-20 22:32:37 +0545

    co

--- php/doctordashboard.php
+++ php/doctordashboard.php
@@ -42,11 +42,40 @@
                         <span class="text">Update </span>
                     </div>
 </div>
+<div class="DAT">
+    <h2 id="time"></h2>
+    <h3 id="day"></h3>
+    <h3 id="date"></h3>
+  </div>
                 </div>
             </div>
             </section>
             <script src="script.js">
-
+// Function to update the time, day, and date
+function updateTime() {
+      const now = new Date();
+      // Get the current time (hours, minutes, and seconds)
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const seconds = now.getSeconds().toString().padStart(2, '0');
+      const timeString = ${hours}:${minutes}:${seconds};
+      // Get the current day (Sunday, Monday, etc.)
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const dayString = days[now.getDay()];
+      // Get the current date (day, month, and year)
+      const date = now.getDate().toString().padStart(2, '0');
+      const month = (now.getMonth() + 1).toString().padStart(2, '0');
+      const year = now.getFullYear();
+      const dateString = ${date}/${month}/${year};
+      // Update the HTML content
+      document.getElementById('time').textContent = Current Time: ${timeString};
+      document.getElementById('day').textContent = Current Day: ${dayString};
+      document.getElementById('date').textContent = Current Date: ${dateString};
+    }
+    // Update the time every second
+    setInterval(updateTime, 1000);
+    // Initial update when the page loads
+    updateTime();
 const body = document.querySelector("body"),
       modeToggle = body.querySelector(".mode-toggle");
       let getStatus = localStorage.getItem("status");
@@ -61,6 +90,7 @@ sidebarToggle.addEventListener("click", () => {
         localStorage.setItem("status", "open");
     }
 })
+
 </script>
 </body>
 </html>
