8c71cfc405f0766e89796c22fbdaff6bcd591377
yunishs <yunishshr1@gmail.com>
2023-09-20 11:36:19 +0545

    session

--- database/connection.php
+++ database/connection.php
@@ -1,16 +1,22 @@
 <?php
-    $servername= 'localhost';
-    $username= 'root';
-    $password= '';
+    // $servername= 'localhost';
+    // $username= 'root';
+    // $password= '';
 
-    //Connecting to database
-    try{
-        $conn = new PDO("mysql:host=$servername;dbname=login-info", $username, $password);
-        //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connection Successfully.';
-    }catch(\Exception $e){
-        $error_message=$e->getMessage();
+    // //Connecting to database
+    // try{
+    //     $conn = new PDO("mysql:host=$servername;dbname=login-info", $username, $password);
+    //     //set the PDO error mode to exception
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo 'Connection Successfully.';
+    // }catch(\Exception $e){
+    //     $error_message=$e->getMessage();
+    // }
+    $con = mysqli_connect('localhost','root','','login-info');
+    if (mysqli_connect_errno())
+    {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
+    ?>
 
 ?>
