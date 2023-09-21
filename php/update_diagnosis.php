227fb4956714f0fec684ba3f442312b5124b9856
prasi09 <prasiddha.shiwakoti@gmail.com>
2023-09-22 00:43:42 +0545

    int

--- php/update_diagnosis.php
+++ php/update_diagnosis.php
@@ -1,87 +1,20 @@
 <?php
-
     include ("..\database\connect.php");
-   
-    //initializing variables
-    $fname=$mname=$lname=$age=$gender=$nationality=$bloodgroup=$address=$contact=$email=$pat_description="";
-    $fnameErr=$mnameErr=$lnameErr=$ageErr=$genderErr=$nationalityErr=$bloodgroupErr=$addressErr=$contactErr=$emailErr=$pat_descriptionErr=null;
-// try{
-    if(isset($_POST['enter']))
-    {
-        function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-          }
-       
-          {
-            $symptoms=test_input($_POST['symptoms']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
-                $symptomsErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $test_conducted=test_input($_POST['test_conducted']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
-                $test_conductedErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $medication=test_input($_POST['medication']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
-                $medicationErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        function function_alert($message) {
-            echo "<script>alert('$message');</script>";
-        }
-        if(  empty($symptomsErr) && empty($test_conductedErr) && empty($medication))
-    {
-        $sql="insert into patient_info(symptoms,test_conducted_medication) values ('$symptoms','$test_conducted','$medication')";
-        $result=mysqli_query($con,$sql);
-        if($result){
-            // function_alert("Data inserted successfully");
-            header('Location: pat_view.php');
-        }
-        else
-        {
-            // function_alert("Data couldn't be inserted successfully");
-            die(mysqli_error($con));
-            
-        }
-    }
-    else
-    {
-        if(!empty($dateandtimeErr)){
-            function_alert($datenadtimeErr);
-        } 
-        if(!empty($symptomsErr)){
-            function_alert($symptomsErr);
-        }     
-        if(!empty($test_conductedErr)){
-            function_alert($test_conductedErr);
-        }
-        if(!empty($medicationErr)){
-            function_alert($medicationErr);
-        }       
-    }
-}
+    $pid='';
 ?>
 
-
-?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <!-- <header>
-        <div class="logosec">
-            <img src="..\images\MedHost.png"
-            class="icn menuicn"
-            id="menuicn">
-            <div class="logo">MedHost</div>
-        </div>
-    </header> -->
-    
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible"content="IE=edge">
+	<meta name="viewport"	content="width=device-width, initial-scale=1.0">
+	<title>Update Diagnosis</title>
+	<link rel="stylesheet"  href="..\public\update_diag_prog.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="..\images\MedHost.png">
+</head>
+<body>
     <?php
         $sql="SELECT * from patient_info where fname='Ram' AND contact='9844656849' LIMIT 1";
 		$result=mysqli_query($con,$sql);
@@ -101,78 +34,39 @@
                 {
                     $name=$fname." ".$mname." ".$lname;
                 }
-                $dateandtime=$row['dateandtime'];
-                $symptoms=$row['symptoms'];
-                $test_conducted=$row['test_conducted'];
-                $medication=$row['medication'];
             }
         }
     ?>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diagnosis page</title>
-    <link rel="stylesheet" href="..\public\update_diag_prog.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <link rel="icon" type="image/png" href="..\images\MedHost.png">
-</head>
-<body>
-    <header>
-        <div class="logosec">
-            <img src="..\images\MedHost.png"
-                class="icn menuicn"
-                id="menuicn">
-                <div class="logo">MED-Host</div>
-        </div>
-    </header>
-    <section class="container">
     <table>
-            <tr>
-                <th>Patient-ID</th>
-                <td class="pid"><?= $pid?></td>
-                <th>Patient's Name</th>
-                <td><?= $name?></td>
-            </tr>
+        <tr>
+            <th>Patient ID</th>
+            <td><?= $pid?></td>
+            <th>Patient Name</th>
+            <td><?= $name?></td>
+        </tr>
     </table>
-    </div>  
-    <form method="POST">
-        <div class="input-box">
-            <h3>Diagnosis:</h3>
-            <form action="#" class="form">
-            <!-- <form action="" method="post"></form> -->
-            <div class="column">
+    <section class="container">
+        <form class="form" method="POST">
             <div class="input-box">
-                <label>Date:</label>
-                <input type="datetime-local" id="dateandtime" name="dateandtime" value=<?php echo $dateandtime; ?>>
+                <label>Date</label><br>
+                <input type="datetime-local" id="date" name="date" value="<?php echo $date; ?>">
             </div>
             <div class="input-box">
-                <label>Symptoms</label>
-                <input type="text" id="symptoms" placeholder="Enter the patient's symptoms" name="symptoms" value=<?php echo $symptoms; ?>>
+                <label>Symptoms</label><br>
+                <textarea type="text" class="symptoms" id="symptoms" name="symptoms" placeholder="Enter the symptoms patient currently has.." value="<?php echo $symptoms; ?>"></textarea>
             </div>
-        </div>
-        <div class="input-box">
-          <label>Test Conducted:</label>
-          <input type="text_conducted" class="text_conducted" id="text_conducted" name="text_conducted" placeholder="Enter the patient's test conducted" value=<?php echo $test_conducted; ?>>
-        </div>
-        <div class="input-box">
-          <label>Medication:</label>
-          <input type="medication" class="medication" id="medication" name="medication" placeholder="Enter the patient's medications" value=<?php echo $medication; ?>>
-        </div>
-        <button type="enter" class="enter-btn" name="enter">ENTER</button>
-      </form>
+            <div class="input-box">
+                <label>Test Conducted</label><br>
+                <textarea type="text" class="test_conducted" id="test_conducted" name="test_conducted" placeholder="Enter the tests conducted.." value="<?php echo $test_conducted; ?>"></textarea>
+            </div>
+            <div class="input-box">
+                <label>Medication</label><br>
+                <textarea type="text" class="medication" id="medication" name="medication" placeholder="Enter the prescribe medications.." value="<?php echo $medication; ?>""></textarea>
+            </div>
+            <button type="enter" class="enter-btn" name="enter">ENTER</button>
+        </form>
     </section>
-    <script>
-    window.onscroll = function() {myFunction()};
-    var header = document.getElementById("myHeader");
-    var sticky = header.offsetTop;function myFunction() 
-    {
-        if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
-        }
-    }
-    </script>
+    
+
+
 </body>
-</html>
--- php/update_prognosis.php
+++ php/update_prognosis.php
@@ -1,84 +1,20 @@
 <?php
-
     include ("..\database\connect.php");
-   
-    //initializing variables
-    $fname=$mname=$lname=$age=$gender=$nationality=$bloodgroup=$address=$contact=$email=$pat_description="";
-    $fnameErr=$mnameErr=$lnameErr=$ageErr=$genderErr=$nationalityErr=$bloodgroupErr=$addressErr=$contactErr=$emailErr=$pat_descriptionErr=null;
-// try{
-    if(isset($_POST['enter']))
-    {
-        function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-          }
-       
-          {
-            $symptoms=test_input($_POST['symptoms']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
-                $symptomsErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $test_conducted=test_input($_POST['test_conducted']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
-                $test_conductedErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $medication=test_input($_POST['medication']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
-                $medicationErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        function function_alert($message) {
-            echo "<script>alert('$message');</script>";
-        }
-        if(  empty($symptomsErr) && empty($test_conductedErr) && empty($medication))
-    {
-        $sql="insert into patient_info(symptoms,test_conducted_medication) values ('$symptoms','$test_conducted','$medication')";
-        $result=mysqli_query($con,$sql);
-        if($result){
-            // function_alert("Data inserted successfully");
-            header('Location: pat_view.php');
-        }
-        else
-        {
-            // function_alert("Data couldn't be inserted successfully");
-            die(mysqli_error($con));
-            
-        }
-    }
-    else
-    {
-        if(!empty($dateandtimeErr)){
-            function_alert($datenadtimeErr);
-        } 
-        if(!empty($condition_of_patientErr)){
-            function_alert($condition_of_patientErr);
-        }
-        if(!empty($remarksErr)){
-            function_alert($remarksErr);
-        }       
-    }
-}
+    $pid='';
 ?>
 
-
-?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <!-- <header>
-        <div class="logosec">
-            <img src="..\images\MedHost.png"
-            class="icn menuicn"
-            id="menuicn">
-            <div class="logo">MedHost</div>
-        </div>
-    </header> -->
-    
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible"content="IE=edge">
+	<meta name="viewport"	content="width=device-width, initial-scale=1.0">
+	<title>Update Diagnosis</title>
+	<link rel="stylesheet"  href="..\public\update_diag_prog.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="icon" type="image/x-icon" href="..\images\MedHost.png">
+</head>
+<body>
     <?php
         $sql="SELECT * from patient_info where fname='Ram' AND contact='9844656849' LIMIT 1";
 		$result=mysqli_query($con,$sql);
@@ -98,74 +34,35 @@
                 {
                     $name=$fname." ".$mname." ".$lname;
                 }
-                $dateandtime=$row['dateandtime'];
-                $condition_of_patient=$row['condition_of_patient'];
-                $remarks=$row['remarks'];
-                
             }
         }
     ?>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prognosis page</title>
-    <link rel="stylesheet" href="..\public\update_diag_prog.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <link rel="icon" type="image/png" href="..\images\MedHost.png">
-</head>
-<body>
-    <header>
-        <div class="logosec">
-            <img src="..\images\MedHost.png"
-                class="icn menuicn"
-                id="menuicn">
-                <div class="logo">MED-Host</div>
-        </div>
-    </header>
-    <section class="container">
     <table>
-            <tr>
-                <th>Patient-ID</th>
-                <td class="pid"><?= $pid?></td>
-                <th>Patient's Name</th>
-                <td><?= $name?></td>
-            </tr>
+        <tr>
+            <th>Patient ID</th>
+            <td><?= $pid?></td>
+            <th>Patient Name</th>
+            <td><?= $name?></td>
+        </tr>
     </table>
-    </div>  
-    <form method="POST">
-        <div class="input-box">
-            <h3>Prognosis:</h3>
-            <form action="#" class="form">
-            <!-- <form action="" method="post"></form> -->
-            <div class="column">
+    <section class="container">
+        <form class="form" method="POST">
             <div class="input-box">
-                <label>Date:</label>
-                <input type="datetime-local" id="dateandtime" name="dateandtime" value=<?php echo $dateandtime; ?>>
+                <label>Date</label><br>
+                <input type="datetime-local" id="date" name="date" value="<?php echo $date; ?>">
             </div>
             <div class="input-box">
-                <label>Condition Of Patient:</label>
-                <input type="text" id="condition_of_patient" placeholder="Enter the patient's condition" name="condition_of_patient" value=<?php echo $condition_of_patient; ?>>
+                <label>Condition of Patient</label><br>
+                <textarea type="text" class="condition_of_pat" id="condition_of_pat" name="condition_of_pat" placeholder="Enter the medical condition of patient.." value="<?php echo $condition_of_pat; ?>"></textarea>
             </div>
-        </div>
-        <div class="input-box">
-          <label>Remarks:</label>
-          <input type="remarks" class="remarks" id="remarks" name="remarks" placeholder="Enter the remarks fro patient" value=<?php echo $remarks; ?>>
-        </div>
-        <button type="enter" class="enter-btn" name="enter">ENTER</button>
-      </form>
+            <div class="input-box">
+                <label>Remarks</label><br>
+                <textarea type="text" class="remarks" id="remarks" name="remarks" placeholder="Enter the remarks.." value="<?php echo $remarks; ?>"></textarea>
+            </div>
+            <button type="enter" class="enter-btn" name="enter">ENTER</button>
+        </form>
     </section>
-    <script>
-    window.onscroll = function() {myFunction()};
-    var header = document.getElementById("myHeader");
-    var sticky = header.offsetTop;function myFunction() 
-    {
-        if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
-        }
-    }
-    </script>
+    
+
+
 </body>
-</html>
