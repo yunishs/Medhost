eb17e8db0126b24ad64914808901d4da42ae71f1
Fchhetry <poojafuyal20@gmail.com>
2023-09-21 19:01:21 +0545

    and

--- php/update_diagnosis.php
+++ php/update_diagnosis.php
@@ -14,7 +14,26 @@
             $data = htmlspecialchars($data);
             return $data;
           }
-          function function_alert($message) {
+       
+          {
+            $symptoms=test_input($_POST['symptoms']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
+                $symptomsErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        {
+            $test_conducted=test_input($_POST['test_conducted']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
+                $test_conductedErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        {
+            $medication=test_input($_POST['medication']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
+                $medicationErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        function function_alert($message) {
             echo "<script>alert('$message');</script>";
         }
         if(  empty($symptomsErr) && empty($test_conductedErr) && empty($medication))
@@ -34,24 +53,6 @@
     }
     else
     {
-          {
-            $symptoms=test_input($_POST['symptoms']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
-                $symptomsErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $test_conducted=test_input($_POST['test_conducted']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
-                $test_conductedErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $medication=test_input($_POST['medication']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
-                $medicationErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
         if(!empty($dateandtimeErr)){
             function_alert($datenadtimeErr);
         } 
@@ -72,14 +73,6 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-   <head>
-      <meta charset="utf-8">
-      <title>Update Diagnosis</title>
-      <link rel="stylesheet" href="..\public\update_diag_prog.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-      <link rel="icon" type="image/x-icon" href="..\images\MedHost.png">
-   </head>
-<body>
     <!-- <header>
         <div class="logosec">
             <img src="..\images\MedHost.png"
@@ -115,12 +108,11 @@
             }
         }
     ?>
-    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration_page</title>
+    <title>Diagnosis page</title>
     <link rel="stylesheet" href="..\public\update_diag_prog.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="icon" type="image/png" href="..\images\MedHost.png">
@@ -134,7 +126,7 @@
                 <div class="logo">MED-Host</div>
         </div>
     </header>
-    <div class="container">
+    <section class="container">
     <table>
             <tr>
                 <th>Patient-ID</th>
@@ -147,39 +139,40 @@
     <form method="POST">
         <div class="input-box">
             <h3>Diagnosis:</h3>
+            <form action="#" class="form">
             <!-- <form action="" method="post"></form> -->
-            <div class="row">
-                <div class="cols-25">
-                <label class="dateandtime">Date:</label>
-                </div>
-                <div class="col-35">
-                    <input type="integer" class="dateandtime" id="dateandtime" name="dateandtime" placeholder="Enter the date:" value=<?php echo $dateandtime; ?>>
-                </div>
-                </div>
-            <div class="row">
-                <div class="col-25" >
-                <label class="symptoms">Symptoms: </label>
-                </div> 
-                <div class="col-35">
-                    <input type="text" class="symptoms" id="symptoms" name="symptoms" placeholder="Enter the patient's symptoms" value=<?php echo $symptoms; ?>>
-                </div>
-    </div>
-                <div class="row">
-                <div class="col-25" >
-                <label class="test_conducted">Test conducted: </label>
-                </div>
-                <div class="col-35">
-                    <input type="text" class="test_conducted" id="test_conducted" name="test_conducted" placeholder="Enter the test's conducted" value=<?php echo $test_conducted; ?>>
-                </div>
-    </div>
-                <div class="row">
-                <div class="col-25" >
-                <label class="medication">Medication: </label>
-                </div>
-                <div class="col-35">
-                    <input type="text" class="medication" id="medication" name="medication" placeholder="Enter the patient's medications" value=<?php echo $medication; ?>>
-                </div>
-    </div>
-
+            <div class="column">
+            <div class="input-box">
+                <label>Date:</label>
+                <input type="datetime-local" id="dateandtime" name="dateandtime" value=<?php echo $dateandtime; ?>>
+            </div>
+            <div class="input-box">
+                <label>Symptoms</label>
+                <input type="text" id="symptoms" placeholder="Enter the patient's symptoms" name="symptoms" value=<?php echo $symptoms; ?>>
+            </div>
+        </div>
+        <div class="input-box">
+          <label>Test Conducted:</label>
+          <input type="text_conducted" class="text_conducted" id="text_conducted" name="text_conducted" placeholder="Enter the patient's test conducted" value=<?php echo $test_conducted; ?>>
+        </div>
+        <div class="input-box">
+          <label>Medication:</label>
+          <input type="medication" class="medication" id="medication" name="medication" placeholder="Enter the patient's medications" value=<?php echo $medication; ?>>
+        </div>
+        <button type="enter" class="enter-btn" name="enter">ENTER</button>
+      </form>
+    </section>
+    <script>
+    window.onscroll = function() {myFunction()};
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;function myFunction() 
+    {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+    </script>
 </body>
 </html>
