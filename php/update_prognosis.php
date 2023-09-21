787b60a2ae7fb4b856a59b13146ecc138f7f1d0d
Fchhetry <poojafuyal20@gmail.com>
2023-09-21 21:44:00 +0545

    and

--- php/update_prognosis.php
+++ php/update_prognosis.php
@@ -14,7 +14,26 @@
             $data = htmlspecialchars($data);
             return $data;
           }
-          function function_alert($message) {
+       
+          {
+            $symptoms=test_input($_POST['symptoms']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
+                $symptomsErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        {
+            $test_conducted=test_input($_POST['test_conducted']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
+                $test_conductedErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        {
+            $medication=test_input($_POST['medication']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
+                $medicationErr = "Only letters and whitespace allowed in specialization";
+            }    
+        }  
+        function function_alert($message) {
             echo "<script>alert('$message');</script>";
         }
         if(  empty($symptomsErr) && empty($test_conductedErr) && empty($medication))
@@ -34,33 +53,15 @@
     }
     else
     {
-          {
-            $symptoms=test_input($_POST['symptoms']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$symptoms)) {
-                $symptomsErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $test_conducted=test_input($_POST['test_conducted']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$test_conducted)) {
-                $test_conductedErr = "Only letters and whitespace allowed in specialization";
-            }    
-        }  
-        {
-            $medication=test_input($_POST['medication']);
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$medication)) {
-                $medicationErr = "Only letters and whitespace allowed in specialization";
-            }    
-        } 
         if(!empty($dateandtimeErr)){
             function_alert($datenadtimeErr);
-        }    
+        } 
         if(!empty($condition_of_patientErr)){
             function_alert($condition_of_patientErr);
-        }     
+        }
         if(!empty($remarksErr)){
             function_alert($remarksErr);
-        }      
+        }       
     }
 }
 ?>
@@ -69,14 +70,6 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-   <head>
-      <meta charset="utf-8">
-      <title>Update Diagnosis</title>
-      <link rel="stylesheet" href="..\public\update_diag_prog.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-      <link rel="icon" type="image/x-icon" href="..\images\MedHost.png">
-   </head>
-<body>
     <!-- <header>
         <div class="logosec">
             <img src="..\images\MedHost.png"
@@ -105,19 +98,18 @@
                 {
                     $name=$fname." ".$mname." ".$lname;
                 }
-                // $dateandtime=$row['dateandtime'];
-                // $condition_of_patient=$row['condition_of_patient'];
-                // $remarks=$row['remarks'];
+                $dateandtime=$row['dateandtime'];
+                $condition_of_patient=$row['condition_of_patient'];
+                $remarks=$row['remarks'];
                 
             }
         }
     ?>
-    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration_page</title>
+    <title>Prognosis page</title>
     <link rel="stylesheet" href="..\public\update_diag_prog.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="icon" type="image/png" href="..\images\MedHost.png">
@@ -131,7 +123,7 @@
                 <div class="logo">MED-Host</div>
         </div>
     </header>
-    <div class="container">
+    <section class="container">
     <table>
             <tr>
                 <th>Patient-ID</th>
@@ -144,30 +136,36 @@
     <form method="POST">
         <div class="input-box">
             <h3>Prognosis:</h3>
+            <form action="#" class="form">
             <!-- <form action="" method="post"></form> -->
-            <div class="row">
-                <div class="cols-25">
-                <label class="dateandtime">Date:</label>
-                </div>
-                <div class="col-35">
-                    <input type="integer" class="dateandtime" id="dateandtime" name="dateandtime" placeholder="Enter the date:" value=<?php echo $dateandtime; ?>>
-                </div>
-                </div>
-            <div class="row">
-                <div class="col-60" >
-                <label class="condition_of_patient">Condition Of Patient: </label>
-                </div> 
-                <div class="col-75">
-                    <input type="text" class="condition_of_patient" id="condition_of_patient" name="condition_of_patient" placeholder="Enter the condition of patient" value=<?php echo $condition_of_patient; ?>>
-                </div>
-    </div>
-                <div class="row">
-                    <div class="col-60" >
-                        <label class="remarks">Remark: </label>
-                    </div>
-                    <div class="col-75">
-                        <input type="text" class="remarks" id="remarks" name="remarks" placeholder="Enter the remarks" value=<?php echo $remarks; ?>>
-                    </div>
-                </div>
+            <div class="column">
+            <div class="input-box">
+                <label>Date:</label>
+                <input type="datetime-local" id="dateandtime" name="dateandtime" value=<?php echo $dateandtime; ?>>
+            </div>
+            <div class="input-box">
+                <label>Condition Of Patient:</label>
+                <input type="text" id="condition_of_patient" placeholder="Enter the patient's condition" name="condition_of_patient" value=<?php echo $condition_of_patient; ?>>
+            </div>
+        </div>
+        <div class="input-box">
+          <label>Remarks:</label>
+          <input type="remarks" class="remarks" id="remarks" name="remarks" placeholder="Enter the remarks fro patient" value=<?php echo $remarks; ?>>
+        </div>
+        <button type="enter" class="enter-btn" name="enter">ENTER</button>
+      </form>
+    </section>
+    <script>
+    window.onscroll = function() {myFunction()};
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;function myFunction() 
+    {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+    </script>
 </body>
 </html>
