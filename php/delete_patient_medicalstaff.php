2c8faf041dcfba9d7aa1e5215661f8ec59b10867
prasi09 <prasiddha.shiwakoti@gmail.com>
2023-09-27 23:20:35 +0545

    int

--- php/delete_patient_medicalstaff.php
+++ php/delete_patient_medicalstaff.php
@@ -5,7 +5,7 @@ include '..\database\connect.php';
 
 if(isset($_GET['deleteid'])){
     $id=$_GET['deleteid'];
-    $sql1="UPDATE room JOIN patient_info ON room.room_id=patient_info.roomid_fk SET room.alloc_stat = '0' WHERE patient_info.roomid_fk=$id";
+    $sql1="UPDATE room JOIN patient_info ON room.room_id=patient_info.roomid_fk SET room.alloc_stat = '0' WHERE patient_info.pid=$id";
     $result1=mysqli_query($con,$sql1);
     $sql="DELETE FROM patient_info WHERE pid=$id";
     $result=mysqli_query($con,$sql);
